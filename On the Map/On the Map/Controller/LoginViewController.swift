67da4e08166e6ea2d7fe85e88b39aa99a01c026f
John Berndt <johnwberndt@gmail.com>
2019-02-10 20:25:33 -0500

    Initial Commit

---
+++ On the Map/On the Map/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  On the Map
+//
+//  Created by John Berndt on 2/10/19.
+//  Copyright © 2019 John Berndt. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
