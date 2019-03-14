c4e247b3c0f241831607f9cef219208ccf0fb0b1
John Berndt <johnwberndt@gmail.com>
2019-03-13 22:19:13 -0400

    Added Navigation Controller and MapKit scenes; Map scene appears on login

---
+++ On the Map/On the Map/Controller/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  On the Map
+//
+//  Created by John Berndt on 3/13/19.
+//  Copyright © 2019 John Berndt. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
